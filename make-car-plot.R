07c720c9e889d00a4374b71d69deffc62a1a80f0
Stephanie Saab <stephaniesaab5@gmail.com>
2021-06-24 13:42:08 -0400

    Final project addition

--- make-car-plot.R
+++ make-car-plot.R
@@ -1,3 +1,12 @@
 library(distill)
 library(tidyverse)
 library(ggthemes)
+car_plot<-mtcars %>% 
+  select(mpg,wt) %>% 
+  ggplot(aes(wt,mpg))+
+  geom_point()+
+  geom_smooth(method="lm",formula=y~x,se=F)+
+  theme_clean()+
+  labs(title="Fuel Efficiency and Weight", subtitle = "Heavier cars are less fuel efficient",x="Weight (1000s of lbs)", y="MPG", caption="Henderson and Vellerman (1981)")
+write_rds(car_plot,"car_plot.rds")
+
